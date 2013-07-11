3894c0b6094d272a422af312e0dd2389483a2f88
Jesse CXElegance <contact@cxelegance.com>
2013-07-11 18:53:22 +1200

    added some callbacks and back button for testing

--- js/master.js
+++ js/master.js
@@ -1,5 +1,14 @@
 // Put all your page JS here
 
 $(function () {
-    $('#slickQuiz').slickQuiz();
+    $('#slickQuiz').slickQuiz({
+            animationCallbacks: {
+                setup: function () {console.log ('setup complete');},
+                start: function () {console.log ('start complete');},
+                checkSubmit: function () {console.log ('check or submit complete');},
+                back: function () {console.log ('back complete');},
+                next: function () {console.log ('next complete');}
+            },
+	backButtonText: 'back'
+	});
 });
