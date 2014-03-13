3843d346afe9621aa955b7e8754e50b9431c1770
Jesse CXElegance <contact@cxelegance.com>
2014-03-12 23:43:35 -0300

    testing latest refactoring; testing animationCallbacks plus events

--- js/master.js
+++ js/master.js
@@ -2,24 +2,23 @@
 
 $(function () {
     $('#slickQuiz').slickQuiz({
-		callbacks: {
-			animations: {
-				setupQuiz: function () {console.log ('setupQuiz complete');},
-            	startQuiz: function () {console.log ('startQuiz complete');},
-            	resetQuiz: function () {console.log ('resetQuiz complete');},
-            	checkAnswer: function () {console.log ('checkAnswer complete');},
-            	nextQuestion: function () {console.log ('nextQuestion complete');},
-            	backToQuestion: function () {console.log ('backToQuestion complete');},
-            	completeQuiz: function () {console.log ('completeQuiz complete');}
-			},
-			events: {
-				completeQuiz: function (options) {
-					console.log ('quiz completed with:' +
-							(options && options.questionCount ? options.questionCount : 'null') +
-									' & ' +
-									(options && options.score ? options.score : 'null')
-							);
-				}
+		animationCallbacks: {
+			setupQuiz: function () {console.log ('setupQuiz animation complete');},
+        	startQuiz: function () {console.log ('startQuiz animation complete');},
+        	resetQuiz: function () {console.log ('resetQuiz animation complete');},
+        	checkAnswer: function () {console.log ('checkAnswer animation complete');},
+        	nextQuestion: function () {console.log ('nextQuestion animation complete');},
+        	backToQuestion: function () {console.log ('backToQuestion animation complete');},
+        	completeQuiz: function () {console.log ('completeQuiz animation complete');}
+		},
+		events: {
+			onStartQuiz: function () {console.log ('quiz started');},
+			onCompleteQuiz: function (options) {
+				console.log ('quiz completed with feedback args: ' +
+						(options && options.questionCount ? options.questionCount : 'null') +
+								' & ' +
+								(options && options.score ? options.score : 'null')
+						);
 			}
 		},
     	backButtonText: 'back',
